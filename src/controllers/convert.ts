8a0188a046624587891c6114c35dc6766481cda4
udayvunnam <uday4vunnam@gmail.com>
2019-05-23 18:32:29 +0530

    fix: using res.json instead of res.end

--- src/controllers/convert.ts
+++ src/controllers/convert.ts
@@ -1,21 +1,15 @@
-import Joi from "@hapi/joi";
-import { NextFunction, Request, Response } from "express";
-import status from "http-status";
-import { convert } from "../services/currency.service";
-import logger from "../util/logger";
-import { convertInputSchema } from "../util/validator";
+import Joi from '@hapi/joi';
+import { NextFunction, Request, Response } from 'express';
+import status from 'http-status';
+import { convert } from '../services/currency.service';
+import logger from '../util/logger';
+import { convertInputSchema } from '../util/validator';
 
-export let postConvert = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export let postConvert = async (req: Request, res: Response, next: NextFunction) => {
   const { error } = Joi.validate(req.body, convertInputSchema);
   if (error) {
     logger.error(error);
-    res
-      .status(status.METHOD_NOT_ALLOWED)
-      .end({ error, message: "Invalid input" });
+    res.status(status.METHOD_NOT_ALLOWED).json({ error, message: 'Invalid input' });
   }
 
   const { from, to, value } = req.body;
